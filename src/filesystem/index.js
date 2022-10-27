7a8c80f63867e5b87deb7940e36e984ae249c934
joao.gloria <joao.gloria@hopin.to>
2022-10-27 18:29:28 +0100

    🧹 remove .js output files from git

--- src/filesystem/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Folder = exports.File = void 0;
-var file_1 = require("./file");
-Object.defineProperty(exports, "File", { enumerable: true, get: function () { return file_1.File; } });
-var folder_1 = require("./folder");
-Object.defineProperty(exports, "Folder", { enumerable: true, get: function () { return folder_1.Folder; } });
