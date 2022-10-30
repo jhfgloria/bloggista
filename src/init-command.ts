0cfcb5d5db0e594778dfbe9c1bbb69a64b72b342
Volodymyr Balytskyy <volodymyr.balytskyy@gmail.com>
2022-10-30 10:51:47 +0000

    🐛 fix correctly reference CSS file and remove unused reference

--- src/init-command.ts
+++ src/init-command.ts
@@ -55,8 +55,7 @@ const HTMLTemplate = (name: string) => `<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/bloggista.css">
-    <link rel="stylesheet" href="/custom.css">
+    <link rel="stylesheet" href="./custom.css">
     <title>${name}</title>
   </head>
 
